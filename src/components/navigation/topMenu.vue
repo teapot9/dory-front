6c80ec7f0241efba92049fadb007373f8e237617
Florian Forestier <Florian.Forestier@be-ys.cloud>
2022-10-04 10:50:46 +0200

    openldap

--- src/components/navigation/topMenu.vue
+++ src/components/navigation/topMenu.vue
@@ -2,18 +2,7 @@
   <div>
     <div class="navbar navbar-expand-lg navbar-dark bg-primary">
       <div class="container">
-        <div class="logo logo-ux-white logo-small" style="width:80px;">
-          <router-link class="nav-link nobg" to="/"></router-link>
-        </div>
-        <b-collapse id="nav-collapse" is-nav>
-          <b-navbar-nav>
-            <b-nav-item to="/">
-              Domain Operator - Recover by Yourself
-            </b-nav-item>
-          </b-navbar-nav>
-        </b-collapse>
-
-
+        <router-link class="nav-link nobg" to="/"><img src="images/logo.png" height="54px"/></router-link>
       </div>
     </div>
   </div>
